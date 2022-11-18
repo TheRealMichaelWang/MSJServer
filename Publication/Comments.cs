7c2508b69c3a7540c3beea188aeee162fed91413
KamranKaza <kamrankaza@gmail.com>
2022-11-17 23:24:02 -0800

    More UI Changes

---
+++ Publication/Comments.cs
@@ -0,0 +1,114 @@
+﻿using System.Net;
+using System.Text;
+using MSJServer.HTTP;
+
+namespace MSJServer
+{
+    public class Comment
+    {
+        public static List<Comment> LoadComments(Guid discussionGuid, bool excludeRevisions)
+        {
+            if (!File.Exists($"comments/{discussionGuid}.dat"))
+                return new();
+            using(FileStream stream = new FileStream($"comments/{discussionGuid}.dat", FileMode.Open, FileAccess.Read))
+            using(BinaryReader reader = new BinaryReader(stream))
+            {
+                int count = reader.ReadInt32();
+                List<Comment> comments = new(count);
+                for (int i = 0; i < count; i++)
+                {
+                    Comment loadedComment = new Comment(reader);
+                    if(!(excludeRevisions && loadedComment.RevisionRequested))
+                        comments.Add(loadedComment);
+                }
+                return comments;
+            }
+        }
+
+        public static void MakeComment(Guid discussionGuid, Comment comment)
+        {
+            bool exist = true;
+            if (!File.Exists($"comments/{discussionGuid}.dat"))
+            {
+                File.Create($"comments/{discussionGuid}.dat").Close();
+                exist = false;
+            }
+            using(FileStream stream = new FileStream($"comments/{discussionGuid}.dat", FileMode.Open, FileAccess.ReadWrite))
+            {
+                int commentCount = 1;
+                if (exist)
+                {
+                    using (BinaryReader reader = new BinaryReader(stream, Encoding.UTF8, true))
+                        commentCount = reader.ReadInt32();
+                    stream.Position = 0;
+                    commentCount++;
+                }
+                using(BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    writer.Write(commentCount);
+                    stream.Position = stream.Length;
+                    comment.WriteBack(writer);
+                }
+            }
+        }
+
+        public string Sender { get; private set; }
+        public string Content { get; private set; }
+        public DateTime Created { get; private set; }
+        public bool RevisionRequested { get; private set; }
+
+        public Comment(string sender, string content, bool revisionRequested, DateTime created)
+        {
+            Sender = sender;
+            Content = content;
+            Created = created;
+            RevisionRequested = revisionRequested;
+        }
+
+        public Comment(BinaryReader reader) : this(reader.ReadString(), reader.ReadString(), reader.ReadBoolean(), new DateTime(reader.ReadInt64()))
+        {
+
+        }
+
+        public void WriteBack(BinaryWriter writer)
+        {
+            writer.Write(Sender);
+            writer.Write(Content);
+            writer.Write(RevisionRequested);
+            writer.Write(Created.Ticks);
+        }
+
+        public override string ToString() => $"<div><a href=\"/userinfo?username={Sender}\">{Sender}</a><a class=\"text-muted\"> • {Created.ToShortTimeString()}</a><a><span class=\"badge badge-primary mx-1\">{(RevisionRequested ? "Revision Requested" : string.Empty)}</span></a></div>{Content}<hr>";
+    }
+
+    partial class Server
+    {
+        private void HandleCommentRequest(HttpListenerContext context)
+        {
+            Dictionary<string, string> commentInfo = context.Request.GetPOSTData();
+            Guid id = Guid.Parse(commentInfo["id"]);
+            bool requestRevision = commentInfo.ContainsKey("revise") && commentInfo["revise"] == "yes";
+
+            if (!Article.Exists(id))
+            {
+                RespondError(context, $"Failed to Make Comment", $"Are you sure article {id} exists?");
+                return;
+            }
+            Article article = Article.FromFile(id);
+            Account? account = GetLoggedInAccount(context);
+            if (account == null)
+            {
+                RespondError(context, "Failed to Make Comment", "You must be logged in to comment.");
+                return;
+            }
+            else if(requestRevision && account.Permissions < Permissions.Editor)
+            {
+                RespondError(context, "Failed to Make Comment", "Only editors can request revisions.");
+                return;
+            }
+
+            article.MakeComment(new Comment(account.Name, commentInfo["msg"], requestRevision, DateTime.Now));
+            Redirect(context, $"/article?id={id}");
+        }
+    }
+}
---
+++ Publication/FrontPage.cs
@@ -0,0 +1,69 @@
+﻿using MSJServer.HTTP;
+using System.Net;
+using System.Text;
+
+namespace MSJServer
+{
+    partial class Server
+    {
+        private void HandleFrontPageAccess(HttpListenerContext context)
+        {
+            Dictionary<string, string> queryInfo = context.Request.GetGETData();
+
+            DateOnly day = queryInfo.ContainsKey("day") ? DateOnly.FromDayNumber(int.Parse(queryInfo["day"])) : DateOnly.FromDateTime(DateTime.Now);
+            DateOnly nextDay = day.AddDays(1);
+            DateOnly prevDay = day.AddDays(-1);
+
+            Account? account = GetLoggedInAccount(context);
+            string content;
+            bool isEditor = false;
+            if (account == null)
+                content = File.ReadAllText("templates/index.html");
+            else
+            {
+                content = File.ReadAllText("templates/index_signin.html");
+                content = content.Replace("{USERNAME}", account.Name);
+                isEditor = (account.Permissions >= Permissions.Editor);
+            }
+            content = content.Replace("{EXTFLAGS}", queryInfo.ContainsKey("unpub") ? "&unpub=yes" : string.Empty);
+            content = content.Replace("{DATE}", day.ToLongDateString());
+            content = content.Replace("{TODAYTICKS}", day.DayNumber.ToString());
+            content = content.Replace("{TOMMROW}", nextDay.ToShortDateString());
+            content = content.Replace("{TOMMOROWTICKS}", nextDay.DayNumber.ToString());
+            content = content.Replace("{YESTARDAY}", prevDay.ToShortDateString());
+            content = content.Replace("{YESTARDAYTICKS}", prevDay.DayNumber.ToString());
+            content = content.Replace("{INVFLAGS}", queryInfo.ContainsKey("unpub") ? string.Empty : "&unpub=yes");
+            content = content.Replace("{SECTION}", queryInfo.ContainsKey("unpub") ? "Published Works" : "Unpublished Works");
+
+            Guid[] publishedArticles = Article.GetPublishedArticles(day, queryInfo.ContainsKey("unpub"));
+            if (publishedArticles.Length == 0)
+                content = content.Replace("{DATA}", $"<div class=\"alert alert-primary\" role=\"alert\">As of yet, no articles were published on {day.ToLongDateString()}.</div>");
+            else
+            {
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < publishedArticles.Length; i++)
+                {
+                    if (i % 6 == 0)
+                    {
+                        if (i != 0)
+                        {
+                            builder.Append($"</div>");
+                        }
+                        builder.Append($"<div class=\"card-deck mx-auto\">");
+                    }
+                    Article article = Article.FromFile((Guid)publishedArticles.GetValue(i));
+                    builder.Append($"<div class=\"card mb-3\" style=\"max-width:400px\"><img src=\"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png\" class=\"card-img-top\" alt=\"...\"><div class=\"card-body\">");
+                    builder.Append($"<h5 class=\"card-title\">{article.Title}</h5>");
+                    builder.Append($"<p class=\"card-text\">{article.Snippet}...</p>");
+                    builder.Append($"<a class=\"btn\" style=\"background-color:#34A2A2\" href = \"/article?id={article.Id}\">Read More</a>");
+                    if (article.PublishStatus == PublishStatus.UnderReview && isEditor)
+                        builder.Append("<br><b class=\"mt-2 badge badge-warning\">Editor Attention Required!</b>");
+                    builder.Append($"</div><div class=\"card-footer bg-transparent\">By {article.Author}</div></div>");
+                }
+                builder.Append($"</div>");
+                content = content.Replace("{DATA}", builder.ToString());
+            }
+            Respond202(context, content);
+        }
+    }
+}
