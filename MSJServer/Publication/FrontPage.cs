85e4331c85dda1bbb25aa218cc417caa9c8bd1c6
KamranKaza <kamrankaza@gmail.com>
2023-04-10 07:38:45 -0700

    Old stuff  (#4)

--- MSJServer/Publication/FrontPage.cs
+++ MSJServer/Publication/FrontPage.cs
@@ -27,15 +27,10 @@ namespace MSJServer
             }
             content = content.Replace("{EXTFLAGS}", queryInfo.ContainsKey("unpub") ? "&unpub=yes" : string.Empty);
             content = content.Replace("{DATE}", day.ToLongDateString());
-            content = content.Replace("{TODAYTICKS}", day.DayNumber.ToString());
-            content = content.Replace("{TOMMROW}", nextDay.ToShortDateString());
-            content = content.Replace("{TOMMOROWTICKS}", nextDay.DayNumber.ToString());
-            content = content.Replace("{YESTARDAY}", prevDay.ToShortDateString());
-            content = content.Replace("{YESTARDAYTICKS}", prevDay.DayNumber.ToString());
             content = content.Replace("{INVFLAGS}", queryInfo.ContainsKey("unpub") ? string.Empty : "&unpub=yes");
             content = content.Replace("{SECTION}", queryInfo.ContainsKey("unpub") ? "Published Works" : "Unpublished Works");
 
-            Guid[] publishedArticles = Article.GetPublishedArticles(day, queryInfo.ContainsKey("unpub"));
+            Guid[] publishedArticles = Article.GetPublishedArticles(queryInfo.ContainsKey("unpub"));
             if (publishedArticles.Length == 0)
                 content = content.Replace("{DATA}", $"<div class=\"alert alert-primary\" role=\"alert\">As of yet, no articles were published on {day.ToLongDateString()}.</div>");
             else
@@ -54,7 +49,7 @@ namespace MSJServer
                     Article article = Article.FromFile((Guid)publishedArticles.GetValue(i));
                     var random = new Random();
                     var color = String.Format("#{0:X6}", random.Next(0x1000000));
-                    builder.Append($"<div class=\"col-sm\"><div class=\"card mb-3\" style=\"max-width:540px\"><div class=\"row no-gutters\"><div class=\"col-md-4\" style=\"background-color:{color};\"></div><div class=\"col-md-8\"><div class=\"card-body\">");
+                    builder.Append($"<div class=\"col-sm\"><div class=\"card mb-3\" style=\"max-width:540px\"><div class=\"row no-gutters\"><div class=\"col-md-4\" style=\"background-color:#34A2A2;\"></div><div class=\"col-md-8\"><div class=\"card-body\">");
                     builder.Append($"<h5 class=\"card-title\">{article.Title}</h5>");
                     builder.Append($"<p class=\"card-text\">{article.Snippet}...</p>");
                     builder.Append($"<a class=\"btn btn-outline-secondary\" href = \"/article?id={article.Id}\">Read More</a>");
@@ -67,7 +62,10 @@ namespace MSJServer
                         builder.Append("<br><b class=\"mt-2 badge badge-secondary\">Old Revision</b>");
                     else if (article.PublishStatus == PublishStatus.Rejected)
                         builder.Append("<br><b class=\"mt-2 badge badge-danger\">Rejected</b>");
-                    builder.Append($"</div><div class=\"card-footer bg-transparent text-muted\">Written by <a href=\"/userinfo?username={article.Author}\">{article.Author}</a></div></div></div></div></div>");
+                    if (article.PublishStatus == PublishStatus.Published)
+                        builder.Append($"</div><div class=\"card-footer bg-transparent text-muted\">Written by <a href=\"/userinfo?username={article.Author}\">{article.Author}</a><br>{article.PublishTime}</div></div></div></div></div>");
+                    else
+                        builder.Append($"</div><div class=\"card-footer bg-transparent text-muted\">Written by <a href=\"/userinfo?username={article.Author}\">{article.Author}</a><br>{article.UploadTime}</div></div></div></div></div>");
                 }
                 builder.Append($"</div>");
                 content = content.Replace("{DATA}", builder.ToString());
