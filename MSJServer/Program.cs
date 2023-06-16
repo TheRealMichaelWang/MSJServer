d58d79c0a6c6c33c4b7a14fc196a0094b52e9aa3
TheRealMichaelWang <mitchelllee1963@gmail.com>
2023-06-16 15:45:24 -0700

    * Added smtp client that connects to email server based on dns * Doesn't actually work b/c missing spf record in dns

--- MSJServer/Program.cs
+++ MSJServer/Program.cs
@@ -42,17 +42,32 @@
             foreach (string key in accounts.Keys)
                 Console.WriteLine(key);
         }
+
+        public void NotifyUserCommand(string[] args)
+        {
+            if (!accounts.ContainsKey(args[1]))
+            {
+                Console.WriteLine($"No such user {args[1]}.");
+                return;
+            }
+
+            Account account = accounts[args[1]];
+            EmailNotifier.Notify(account, args.Length >= 4 ? args[3] : "MSJ Console-Invoked Notification", args[2]);
+        }
     }
 
     static class Program
     {
+        public static bool IsProduction { get; private set; }
+
         private static Server server = new Server();
         private static bool stop = false;
         private static Dictionary<string, Action<string[]>> commands = new()
         {
             {"stop", Stop},
             {"perm", server.SetPermsCommand},
-            {"users", server.ListUsersCommand}
+            {"users", server.ListUsersCommand},
+            {"notify", server.NotifyUserCommand}
         };
 
         private static void Stop(string[] args)
@@ -61,21 +76,35 @@
             stop = true;
         }
 
-        public static void Main()
+        public static void Main(string[] args)
         {
-            if (!Directory.Exists("articles"))
-                Directory.CreateDirectory("articles");
-            if (!Directory.Exists("comments"))
-                Directory.CreateDirectory("comments");
+            IsProduction = args.Contains("--prod") || !args.Contains("--test");
 
-            Console.WriteLine("Starting...");
-            server.Start();
+            if (IsProduction)
+            {
+                if (!Directory.Exists("articles"))
+                    Directory.CreateDirectory("articles");
+                if (!Directory.Exists("comments"))
+                    Directory.CreateDirectory("comments");
+
+                Console.WriteLine("Starting...");
+                server.Start();
+            }
+            else
+                Console.WriteLine("Skiping server start...");
+
+            server.NotifyUserCommand(new string[] { "notify", "therealmichaelwang", "this is a non-prod test msg." });
+
             while (!stop)
             {
 #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
                 string input = Console.ReadLine();
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-                string[] commandArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string[] commandArgs = input.Split('"')
+                                           .Select((element, index) => index % 2 == 0  // If even index
+                                           ? element.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)  // Split the item
+                                           : new string[] { element })  // Keep the entire item
+                                           .SelectMany(element => element).ToArray();
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
 #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
                 if (commandArgs.Length < 1)
---
+++ MSJServer/Publication/EmailNotifier.cs
@@ -0,0 +1,60 @@
+﻿using DnsClient;
+using DnsClient.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSJServer
+{
+    public static class EmailNotifier
+    {
+        private static MxRecord SMTPServer;
+
+#pragma warning disable CS8618
+        static EmailNotifier()
+#pragma warning restore CS8618 
+        {
+            LookupClient client = new();
+#pragma warning disable CS8600 // Almost always will return answers, gmail.com is gmail
+#pragma warning disable CS8601
+            SMTPServer = (MxRecord)client.Query("gmail.com", QueryType.MX).Answers.MinBy((answer) => ((MxRecord)answer).Preference);
+#pragma warning restore CS8601
+#pragma warning restore CS8600
+
+            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(ValidateRemoteCertificate);
+        }
+
+        private static bool ValidateRemoteCertificate(object sender, X509Certificate? cert, X509Chain? chain, SslPolicyErrors policyErrors) => policyErrors == SslPolicyErrors.RemoteCertificateNameMismatch && (cert != null && cert.Subject.EndsWith("google.com"));
+
+        public static void Notify(Account account, string subject, string body)
+        {
+            try
+            {
+                using (SmtpClient client = new(SMTPServer.Exchange, 25))
+                using (MailMessage message = new MailMessage(new MailAddress("noreply@104.174.17.83", "The Magnolia Street Journal"), new MailAddress(account.Email, account.Name)))
+                {
+                    message.Subject = subject;
+                    message.SubjectEncoding = Encoding.UTF8;
+                    message.Body = body;
+                    message.BodyEncoding = Encoding.UTF8;
+
+                    client.UseDefaultCredentials = false;
+                    client.EnableSsl = true;
+
+                    client.Send(message);
+                }
+            }
+            catch (SmtpException e)
+            {
+                ;
+            }
+        }
+    }
+}
